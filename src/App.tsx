0b5df019ea1ae782a49b2dbe97b9ba764adaa3c2
ezms <emanuelzm@protonmail.com>
2022-03-28 09:50:14 -0300

    feat: add animate container to presentation section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import AppRoutes from './routes';
 import GlobalStyles, { Root } from './styles/global';
 import Animations from './styles/animations';
-import { useEffect } from 'react';
-import Aos from 'aos';
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
-    useEffect(() => {
-        Aos.init();
-    }, []);
-
     return (
         <Root>
             <GlobalStyles />
--- src/components/PresentationSection/index.tsx
+++ src/components/PresentationSection/index.tsx
@@ -12,6 +12,7 @@ import {
 } from './style';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import AnimateContainer from '../AnimateContainer';
 
 const PresentationSection = () => {
     useEffect(() => {
@@ -19,27 +20,32 @@ const PresentationSection = () => {
     }, []);
 
     return (
-        <PresentationSectionContainer>
-            <BackgroundSlideShow autoPlay muted loop>
-                <source src={BackgroundVideo} type="video/webm" />
-            </BackgroundSlideShow>
-            <SocialMediaBox />
-            <PresentationTitleContainer>
-                <SignalsContainer />
-                <PresentationTitle
-                    data-aos="fade-down"
-                    data-aos-duration="3000"
-                >
-                    FullStack Developer
-                </PresentationTitle>
-                <PresentationText data-aos="fade-up" data-aos-duration="3000">
-                    Olá! Meu nome é Emanuel Z. Martins, nos últimos anos comecei
-                    a trabalhar com programação front-end em start-ups e
-                    projetos freelance!
-                </PresentationText>
-            </PresentationTitleContainer>
-            <ScrollDownMessage />
-        </PresentationSectionContainer>
+        <AnimateContainer>
+            <PresentationSectionContainer>
+                <BackgroundSlideShow autoPlay muted loop>
+                    <source src={BackgroundVideo} type="video/webm" />
+                </BackgroundSlideShow>
+                <SocialMediaBox />
+                <PresentationTitleContainer>
+                    <SignalsContainer />
+                    <PresentationTitle
+                        data-aos="fade-down"
+                        data-aos-duration="3000"
+                    >
+                        FullStack Developer
+                    </PresentationTitle>
+                    <PresentationText
+                        data-aos="fade-up"
+                        data-aos-duration="3000"
+                    >
+                        Olá! Meu nome é Emanuel Z. Martins, nos últimos anos
+                        comecei a trabalhar com programação front-end em
+                        start-ups e projetos freelance!
+                    </PresentationText>
+                </PresentationTitleContainer>
+                <ScrollDownMessage />
+            </PresentationSectionContainer>
+        </AnimateContainer>
     );
 };
 
